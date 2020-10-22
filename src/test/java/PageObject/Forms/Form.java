431562ccfe7c572c2906ad822b13f7d4bc14ebf5
Aleksandr A. Pimenov <orio_14@mail.ru>
2020-10-22 22:32:32 +0400

    update Project

--- src/test/java/PageObject/Forms/Form.java
+++ src/test/java/PageObject/Forms/Form.java
@@ -11,6 +11,8 @@ public class Form {
     public Form(WebDriver driver){
         this.driver = driver;
     }
+
+    //Page fields
     private By firstName = By.id("firstName");
     private By lastName = By.id("lastName");
     private By email = By.id("userEmail");
@@ -28,74 +30,102 @@ public class Form {
     private By state = By.id("state");
     private By city = By.cssSelector(" css-1hwfws3");
     private By submitBtn = By.id("submit");
+
+    //Modal fields
     private By modal = By.cssSelector("div.modal-content");
     private By closeBtn = By.id("closeLargeModal");
 
+    //Form methods
     public void fillFrom(){
 
     }
-    public void addName(String name){
+
+    public void addName(String name) {
         driver.findElement(firstName).sendKeys(name);
     }
-    public void addLastName(String lastname){
+
+    public void addLastName(String lastname) {
         driver.findElement(lastName).sendKeys(lastname);
     }
-    public void addEmail(String userEmail){
+
+    public void addEmail(String userEmail) {
         driver.findElement(email).sendKeys(userEmail);
     }
-    public void selectMale(){
+
+    public void selectMale() {
         WebElement radio = driver.findElement(genderMale);
         ((JavascriptExecutor) driver).executeScript("arguments[0].checked = true;", radio);
     }
-    public void selectFemale(){
+
+    public void selectFemale() {
         WebElement radio = driver.findElement(genderFemale);
         ((JavascriptExecutor) driver).executeScript("arguments[0].checked = true;", radio);
     }
-    public void selectOthers(){
+
+    public void selectOthers() {
         WebElement radio = driver.findElement(genderOther);
         ((JavascriptExecutor) driver).executeScript("arguments[0].checked = true;", radio);
     }
-    public void addUserNumber(String number){
+
+    public void addUserNumber(String number) {
         driver.findElement(userNumber).sendKeys(number);
     }
-    public void addSubject(String sub){
+
+    public void addBirthDate(){
+        /* ToDO */
+    }
+
+    public void addSubject(String sub) {
         driver.findElement(subject).sendKeys(sub);
     }
-    public void selectSports(){
-        driver.findElement(sports).click();
+
+    public void selectSports() {
+        WebElement checkBox = driver.findElement(sports);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].checked = true;", checkBox);
     }
-    public void selectReading(){
+
+    public void selectReading() {
         WebElement checkBox = driver.findElement(reading);
         ((JavascriptExecutor) driver).executeScript("arguments[0].checked = true;", checkBox);
     }
-    public void selectMusic(){
-        driver.findElement(music).click();
+
+    public void selectMusic() {
+        WebElement checkBox = driver.findElement(music);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].checked = true;", checkBox);
     }
-    public void uploadPic(String file){
+
+    public void uploadPic(String file) {
         driver.findElement(uploadPicture).sendKeys(file);
     }
-    public void addAddress(String text){
+
+    public void addAddress(String text) {
         driver.findElement(currentAddress).sendKeys(text);
     }
-    public void addState(String stateName){
+
+    public void addState(String stateName) {
         driver.findElement(state).sendKeys(stateName);
     }
-    public void addCity(String cityName){
+
+    public void addCity(String cityName) {
         driver.findElement(city).sendKeys(cityName);
     }
-    public void subButton(){
+
+    public void subButton() {
         driver.findElement(submitBtn).click();
     }
-    public boolean isVisible(){
-        if(driver.findElement(modal).isDisplayed()){
+
+    //Modal methods
+    public boolean isVisible() {
+        if (driver.findElement(modal).isDisplayed()) {
             return true;
-        }else{
+        } else {
             return false;
         }
 
     }
-    public void closeModal(){
-        if(isVisible()){
+
+    public void closeModal() {
+        if (isVisible()) {
             driver.findElement(closeBtn).click();
         }
 
